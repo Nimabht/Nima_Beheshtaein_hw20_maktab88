d4d30fdc403a700e0930aff7a93de7876c279ba3
nimabht <nimabeheshtaein99@gmail.com>
2023-04-30 23:44:47 +0330

    connection to db file

---
+++ 1-1/src/database/connection.ts
@@ -0,0 +1,14 @@
+// import mongoose from "mongoose";
+
+// mongoose
+//   .connect("mongodb://localhost:27017/HW20")
+//   .then(() => {
+//     console.log("MongoDB connected!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const db = mongoose.connection;
+
+// export default db;
